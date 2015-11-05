f927ed22bc86aef73a7a6136610b4841d8f128d9
Rodrigo Agerri <ragerri@apache.org>
2015-11-05 15:54:15 +0100

    catching tcp server exceptions better

--- src/main/java/eus/ixa/ixa/pipe/pos/MorphoTaggerServer.java
+++ src/main/java/eus/ixa/ixa/pipe/pos/MorphoTaggerServer.java
@@ -24,6 +24,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Properties;
@@ -64,7 +65,12 @@ public class MorphoTaggerServer {
     Integer port = Integer.parseInt(properties.getProperty("port"));
     model = properties.getProperty("model");
     outputFormat = properties.getProperty("outputFormat");
+    
+    String kafToString;
     ServerSocket socketServer = null;
+    Socket activeSocket;
+    BufferedReader inFromClient = null;
+    BufferedWriter outToClient = null;
 
     try {
       Annotate annotator = new Annotate(properties);
@@ -72,21 +78,36 @@ public class MorphoTaggerServer {
       socketServer = new ServerSocket(port);
       System.out.println("-> Connected and listening to port " + port);
       while (true) {
-        
-        try (Socket activeSocket = socketServer.accept();
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(activeSocket.getInputStream(), "UTF-8"));
-            BufferedWriter outToClient = new BufferedWriter(new OutputStreamWriter(activeSocket.getOutputStream(), "UTF-8"));) {
-          //System.err.println("-> Received a  connection from: " + activeSocket);
+        try {
+          activeSocket = socketServer.accept();
+          inFromClient = new BufferedReader(new InputStreamReader(activeSocket.getInputStream(), "UTF-8"));
+          outToClient = new BufferedWriter(new OutputStreamWriter(activeSocket.getOutputStream(), "UTF-8"));
           //get data from client
           String stringFromClient = getClientData(inFromClient);
           // annotate
-          String kafToString = getAnnotations(annotator, stringFromClient);
-          // send to server
-          sendDataToServer(outToClient, kafToString);
+          kafToString = getAnnotations(annotator, stringFromClient);
+        } catch (JDOMException e) {
+          kafToString = "\n-> ERROR: Badly formatted NAF document!!\n";
+          sendDataToClient(outToClient, kafToString);
+          continue;
+        } catch (UnsupportedEncodingException e) {
+          kafToString = "\n-> ERROR: UTF-8 not valid!!\n";
+          sendDataToClient(outToClient, kafToString);
+          continue;
+        } catch (IOException e) {
+          kafToString = "\n -> ERROR: Input data not correct!!\n";
+          sendDataToClient(outToClient, kafToString);
+          continue;
         }
-      }
-    } catch (IOException | JDOMException e) {
+        //send data to server after all exceptions and close the outToClient
+        sendDataToClient(outToClient, kafToString);
+        //close the resources
+        inFromClient.close();
+        activeSocket.close();
+      } //end of processing block
+    } catch (IOException e) {
       e.printStackTrace();
+      System.err.println("-> IOException due to failing to create the TCP socket or to wrongly provided model path.");
     } finally {
       System.out.println("closing tcp socket...");
       try {
@@ -107,6 +128,9 @@ public class MorphoTaggerServer {
     try {
       String line;
       while ((line = inFromClient.readLine()) != null) {
+        if (line.matches("<ENDOFDOCUMENT>")) {
+          break;
+        }
         stringFromClient.append(line).append("\n");
         if (line.matches("</NAF>")) {
           break;
@@ -124,12 +148,13 @@ public class MorphoTaggerServer {
    * @param kafToString the string to be processed
    * @throws IOException if io error
    */
-  private void sendDataToServer(BufferedWriter outToClient, String kafToString) throws IOException {
+  private void sendDataToClient(BufferedWriter outToClient, String kafToString) throws IOException {
     outToClient.write(kafToString);
+    outToClient.close();
   }
   
   /**
-   * Named Entity annotator.
+   * MorphoTagger annotator.
    * @param annotator the annotator
    * @param stringFromClient the string to be annotated
    * @return the annotation result
