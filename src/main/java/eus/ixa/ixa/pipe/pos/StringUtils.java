dac2aa6f7464be08f9fe9ad0d63c41941409f503
Rodrigo Agerri <ragerri@apache.org>
2015-12-22 13:53:01 +0100

    adding levenshtein distance

--- src/main/java/eus/ixa/ixa/pipe/pos/StringUtils.java
+++ src/main/java/eus/ixa/ixa/pipe/pos/StringUtils.java
@@ -183,5 +183,181 @@ public final class StringUtils {
     }
     return fileList;
   }
+  
+  
+  /**
+   * Get mininum of three values.
+   * @param a number a
+   * @param b number b
+   * @param c number c
+   * @return the minimum
+   */
+  private static int minimum(int a, int b, int c) {
+      int minValue;
+      minValue = a;
+      if (b < minValue) {
+        minValue = b;
+      }
+      if (c < minValue) {
+        minValue = c;
+      }
+      return minValue;
+  }
+  
+  /**
+   * Computes the Levenshtein distance of two strings in a matrix.
+   * Based on pseudo-code provided here:
+   * https://en.wikipedia.org/wiki/Levenshtein_distance#Computing_Levenshtein_distance
+   * which in turn is based on the paper Wagner, Robert A.; Fischer, Michael J. (1974),
+   * "The String-to-String Correction Problem", Journal of the ACM 21 (1): 168–173
+   * @param wordForm the form
+   * @param lemma the lemma
+   * @return the distance
+   */
+  public static int[][] levenshteinDistance(String wordForm, String lemma) {
+
+    int wordLength = wordForm.length();
+    int lemmaLength = lemma.length();
+    int cost;
+    int[][] distance = new int[wordLength + 1][lemmaLength + 1];
+    
+    if (wordLength == 0) {
+      return distance;
+    }
+    if (lemmaLength == 0) {
+      return distance;
+    }
+    //fill in the rows of column 0
+    for (int i = 0; i <= wordLength; i++) {
+      distance[i][0] = i;
+    }
+    //fill in the columns of row 0
+    for (int j = 0; j <= lemmaLength; j++) {
+      distance[0][j] = j;
+    }
+    //fill in the rest of the matrix calculating the minimum distance
+    for (int i = 1; i <= wordLength; i++) {
+      int s_i = wordForm.charAt(i - 1);
+      for (int j = 1; j <= lemmaLength; j++) {
+        if (s_i == lemma.charAt(j - 1)) {
+          cost = 0;
+        } else {
+          cost = 1;
+        }
+        //obtain minimun distance from calculating deletion, insertion, substitution
+        distance[i][j] = minimum(distance[i - 1][j] + 1, distance[i][j - 1] + 1, distance[i - 1][j - 1] + cost);
+      }
+    }
+    return distance;
+  }
+  
+  public static void getShortestEditScript(String wordForm, String lemma, int[][] distance, StringBuffer permutations) {
+    
+    int n = distance.length;
+    int m = distance[0].length;
+    
+    int wordFormLength = n - 1;
+    int lemmaLength = m - 1;
+    while(true) {
+        
+        if (distance[wordFormLength][lemmaLength] == 0) {
+          break;
+        }
+        if ((lemmaLength > 0 && wordFormLength > 0) && (distance[wordFormLength - 1][lemmaLength - 1] < distance[wordFormLength][lemmaLength])) {
+            permutations.append('R').append(Integer.toString(wordFormLength - 1)).append(wordForm.charAt(wordFormLength - 1)).append(lemma.charAt(lemmaLength - 1));
+            lemmaLength--;
+            wordFormLength--;
+            continue;
+        }
+        if (lemmaLength > 0 && (distance[wordFormLength][lemmaLength - 1] < distance[wordFormLength][lemmaLength])) {
+            permutations.append('I').append(Integer.toString(wordFormLength)).append(lemma.charAt(lemmaLength - 1));
+            lemmaLength--;
+            continue;
+        }
+        if (wordFormLength > 0 && (distance[wordFormLength - 1][lemmaLength] < distance[wordFormLength][lemmaLength])) {
+            permutations.append('D').append(Integer.toString(wordFormLength - 1)).append(wordForm.charAt(wordFormLength - 1));
+            wordFormLength--;
+            continue;
+        }
+        if ((wordFormLength > 0 && lemmaLength > 0) && (distance[wordFormLength - 1][lemmaLength - 1] == distance[wordFormLength][lemmaLength])) {
+            wordFormLength--; lemmaLength--;
+            continue ;
+        }
+        if (wordFormLength > 0 && (distance[wordFormLength - 1][lemmaLength] == distance[wordFormLength][lemmaLength])) {
+            wordFormLength--;
+            continue;
+        }
+        if (lemmaLength > 0 && (distance[wordFormLength][lemmaLength - 1] == distance[wordFormLength][lemmaLength])) {
+            lemmaLength--;
+            continue;
+        }   
+    }
+}
+  /**
+   * Read predicted SES by the lemmatizer model and apply the
+   * permutations to obtain the lemma from the wordForm.
+   * @param wordForm the wordForm
+   * @param permutations the permutations predicted by the lemmatizer model
+   * @return the lemma
+   */
+  public static String decodeShortestEditScript(String wordForm, String permutations) {
+    
+    StringBuffer lemma = new StringBuffer(wordForm).reverse();
+    
+    int permIndex = 0;
+    while(true) {
+        if (permutations.length() <= permIndex) {
+          break;
+        }
+        //read first letter of permutation string
+        char nextOperation = permutations.charAt(permIndex);
+        //go to the next permutation letter
+        permIndex++;
+        if (nextOperation == 'R') {
+            int charIndex = permutations.charAt(permIndex);
+            // go to the next character in the permutation buffer
+            // which is the replacement character
+            permIndex++;
+            char replace = permutations.charAt(permIndex);
+            //go to the next char in the permutation buffer
+            // which is the candidate character
+            permIndex++;
+            char with = permutations.charAt(permIndex);
+            
+            if (lemma.length() <= charIndex) {
+              return wordForm; 
+            }
+            if (lemma.charAt(charIndex) == replace) {
+              lemma.setCharAt(charIndex, with);
+            }
+            //go to next permutation
+            permIndex++;
+            
+        } else if (nextOperation == 'I') {
+            int charIndex = permutations.charAt(permIndex);
+            permIndex++;
+            //character to be inserted
+            char in = permutations.charAt(permIndex);
+        
+            if (lemma.length() < charIndex) {
+              return wordForm; 
+            }
+            lemma.insert(charIndex, in);
+            //go to next permutation
+            permIndex++;
+        } else if (nextOperation == 'D' ) {
+            int charIndex = permutations.charAt(permIndex);
+            if (lemma.length() <= charIndex) {
+              return wordForm;
+            }
+            lemma.deleteCharAt(charIndex);
+            permIndex++;
+            // go to next permutation
+            permIndex++;
+        }
+    }
+    return lemma.reverse().toString();
+}
+
 
 }
